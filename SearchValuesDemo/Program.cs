a44661efc47689c0bebcdc8be8d4c57b2d59fd0e
thecsharper <si@thecsharper.com>
2024-08-22 08:55:31 +0100

    Add project files.

---
+++ SearchValuesDemo/Program.cs
@@ -0,0 +1,9 @@
+﻿using System.Buffers;
+
+var searchValues = SearchValues.Create(new[] { 'a', 'e', 'i', 'o', 'u' });
+
+var vowelResult = ContainsVowel("The quick brown fox jumps over the lazy dog");
+
+Console.WriteLine(vowelResult);
+
+bool ContainsVowel(ReadOnlySpan<char> text) => text.ContainsAny(searchValues);
